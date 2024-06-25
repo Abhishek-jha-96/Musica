38492103d9aa460efbc62adbf901a93e41888ec7
Abhishek Jha <abhishekjha96500@gmail.com>
2024-06-25 15:12:53 +0530

    complete dockerized application

--- app/home.tsx
+++ app/home.tsx
@@ -13,7 +13,7 @@ export default function Music() {
         </div>
       </div>
       <h1 className="py-5 font-mono text-lg font-semibold">Top 3 Tracks</h1>
-      <div className="w-full bg-white px-10 h-44 flex items-center">
+      <div className="w-full bg-white px-10 flex items-center py-2">
         <CardElm />
       </div>
     </div>
--- shared/cardelm.tsx
+++ shared/cardelm.tsx
@@ -7,6 +7,8 @@ import {
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
+import "@/shared/cardelm.css"
+
 
 export default function CardElm() {
   const url = "http://localhost:8000/toptracks/";
@@ -32,12 +34,13 @@ export default function CardElm() {
       {trackData.map((music, index) => (
         <HoverCard key={index}>
           <HoverCardTrigger>
-            <div className="max-w-sm rounded bg-yellow overflow-hidden shadow-lg">
+            <div className="flex flex-col items-center rounded-sm shadow-md p-2 border-2"
+            style={{backgroundColor: '#FFEEA9', borderBlockColor: "rgb(0 0 0 / 0.9)"}}>
               <div
                 style={{ backgroundImage: `url(${music.cover_url})`, width: "150px", height: "100px", backgroundSize: "cover", backgroundPosition: "center"}}
               ></div>
-              <div className="px-6 py-2">
-                <div className="font-bold text-xl mb-2">{music.song_name}</div>
+              <div className="px-6">
+                <div className="font-bold text-xl text-center mb-2" style={{width: "150px", textAlign: "center"}}>{music.song_name}</div>
               </div>
             </div>
           </HoverCardTrigger>
